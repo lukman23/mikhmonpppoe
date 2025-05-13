fd7c4a0709387c14f47f9934392b7c3635fab99b
lukman23 <57512884+lukman23@users.noreply.github.com>
2025-05-13 21:46:19 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,7 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['rumah'] = array ('1'=>'rumah!10.10.10.1','rumah@|@lukman','rumah#|#aWprbw==','rumah%Ultra Network','rumah^dlimas.net','rumah&Rp','rumah*10','rumah(5','rumah)','rumah=10','rumah@!@enable');
+
+$data['pelem'] = array ('1'=>'pelem!103.171.83.49:219','pelem@|@lukman','pelem#|#aWprbw==','pelem%kiyudan.net','pelem^kiyudan.net','pelem&Rp','pelem*10','pelem(1','pelem)','pelem=10','pelem@!@enable');
